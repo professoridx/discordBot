ae296b825b68886e8a79d8f3522a409f3476e0d3
swadhinbiswas <swadhinbiswas.cse@gmail.com>
2024-03-04 01:19:40 +0600

    trying to make better

--- GithubfetchBot/sendwebhook.py
+++ GithubfetchBot/sendwebhook.py
@@ -7,16 +7,34 @@ from typing import Final
 from gitfunction import GetGitHub
 import requests
 
+python="https://download.logo.wine/logo/Python_(programming_language)/Python_(programming_language)-Logo.wine.png"
+java="https://cdn-icons-png.flaticon.com/512/1183/1183618.png"
+javascript="https://cdn-icons-png.flaticon.com/512/919/919828.png"
+c="https://cdn-icons-png.flaticon.com/128/14865/14865856.png"
+r="https://cdn-icons-png.flaticon.com/128/13527/13527880.png"
+scala="https://cdn-icons-png.flaticon.com/128/6132/6132220.png"
+c_plus="https://cdn-icons-png.flaticon.com/128/6132/6132222.png"
+juputernotebook="https://cdn-icons-png.flaticon.com/128/14422/14422032.png"
+html="https://cdn-icons-png.flaticon.com/128/919/919827.png"
+css="https://cdn-icons-png.flaticon.com/128/919/919826.png"
+php="https://cdn-icons-png.flaticon.com/128/919/919830.png"
+sass="https://cdn-icons-png.flaticon.com/128/919/919831.png"
+typescript="https://cdn-icons-png.flaticon.com/128/919/919832.png"
+swift="https://cdn-icons-png.flaticon.com/128/919/919833.png"
+kotlin="https://cdn-icons-png.flaticon.com/128/1892/1892569.png"
+sql="https://cdn-icons-png.flaticon.com/128/2306/2306173.png"
+mongodb="https://www.opc-router.de/wp-content/uploads/2021/03/mongodb_thumbnail.png"
+postgresql="https://w7.pngwing.com/pngs/441/460/png-transparent-postgresql-plain-wordmark-logo-icon-thumbnail.png"
+mariadb="https://cdn-icons-png.flaticon.com/128/5968/5968313.png"
+
+
 load_dotenv()
 WEBHOOK_URL:Final[str]=os.getenv('WEBHOOK_URL')
 
 
-all_massage=GetGitHub.get_repo("python")
-
-
-
-
 async def foo():
+    
+    all_massage=GetGitHub.get_repo("python")
     async with aiohttp.ClientSession() as session:
         webhook = Webhook.from_url(WEBHOOK_URL, session=session)
         
